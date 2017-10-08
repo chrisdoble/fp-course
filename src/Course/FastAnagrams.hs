4a433c1719dd4578b21041e5d8f9c698c9e36f18
Chris Doble <chris@chrisdoble.com>
2017-10-08 12:09:16 +1100

    Complete Course/FastAnagrams.hs

--- src/Course/FastAnagrams.hs
+++ src/Course/FastAnagrams.hs
@@ -8,14 +8,20 @@ import Course.List
 import Course.Functor
 import qualified Data.Set as S
 
+-- I think there's a problem with this exercise. The `Data.Set` import suggests
+-- the solution should use sets, but `NoCaseString` doesn't have an `Ord`
+-- instance which is required for most `Data.Set` functions. The solution below
+-- is case sensitive, but so is the solution in github.com/tonymorris/fp-course.
+
 -- Return all anagrams of the given string
 -- that appear in the given dictionary file.
 fastAnagrams ::
   Chars
   -> Filename
   -> IO (List Chars)
-fastAnagrams =
-  error "todo: Course.FastAnagrams#fastAnagrams"
+fastAnagrams name f =
+  let findAnagrams w = filter (flip S.member w) $ permutations s
+   in findAnagrams . S.fromList . hlist . lines <$> readFile f
 
 newtype NoCaseString =
   NoCaseString {
